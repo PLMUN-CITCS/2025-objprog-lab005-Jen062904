240f82bc98176054fe849c65fa5fed9f679869a6
Jen062904 <Jen.email@example.com>
2025-02-11 11:09:01 +0800

    Submitting OBJPROG Week 02 - Session 01 - Exercise 04

---
+++ MathLibraryDemo.java
@@ -0,0 +1,19 @@
+public class MathLibraryDemo {
+  public static void main(String[] args) {
+
+   double power = Math.pow(2, 3);
+   System.out.println("Power: " + power);
+
+
+   double squareRoot = Math.sqrt(16);
+   System.out.println("Square Root: " + squareRoot);
+
+   long roundedNumber = Math.round(5.7);
+   System.out.println("Rounded Number: " + roundedNumber);
+
+   double randomNumber = Math.random ();
+   System.out.println("Random Number: " + randomNumber);
+
+   }
+
+}
